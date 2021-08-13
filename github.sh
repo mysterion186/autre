174434cffaff92d0d9888ad84b7b498c62a2d39e
Mysterion186 <kowsikan@hotmail.fr>
2021-08-13 17:59:41 +0200

    intégration du fichier python

--- github.sh
+++ github.sh
@@ -1,11 +1,13 @@
 # on prend 1 paramètre le statut du repo (true = privé, false = public)
 
 pwd
+my_var="$(python3 Path/to/the/file/tokens.py)" # variable qui va contenir le token 
 var=$(pwd)
 mydir="$(basename $PWD)"
 echo "$mydir"
 
-curl -H "Authorization: token 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 " --data '{"name":"'$mydir'"}' https://api.github.com/user/repos
+
+curl -H "Authorization: token '$my_var' " --data '{"name":"'$mydir'"}' https://api.github.com/user/repos
 touch .gitignore
 echo .DS_Store > .gitignore
 git init
