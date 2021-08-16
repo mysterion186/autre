1fc9d53e922bf937a2b013e113d947ac7ca684a9
Mysterion186 <kowsikan@hotmail.fr>
2021-08-16 21:48:56 +0200

    mise à jour github.sh

--- github.sh
+++ github.sh
@@ -1,18 +1,19 @@
 # on prend 1 paramètre le statut du repo (true = privé, false = public)
 
 pwd
-my_var="$(python3 Path/to/the/file/tokens.py)" # variable qui va contenir le token 
+my_var="$(python3 /Full/Path/To/The/File/tokens.py)" # variable qui va contenir le token 
 var=$(pwd)
 mydir="$(basename $PWD)"
 echo "$mydir"
-
-
-curl -H "Authorization: token '$my_var' " --data '{"name":"'$mydir'"}' https://api.github.com/user/repos
+echo "$my_var"
+curl -H "Authorization: token "$my_var" " --data '{"name":"'$mydir'","private":'$1'}' https://api.github.com/user/repos
 touch .gitignore
+touch readme.md
 echo .DS_Store > .gitignore
+echo \#"($mydir)" > readme.md
 git init
-git add .
+git add readme.md
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/NOM_UTILISATEUR/"$mydir".git
+git remote add origin https://github.com/USERNAME/"$mydir".git
 git push -u origin main
