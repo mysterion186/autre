16b7f4352c4c48b0df5b738e33ea575521a8f247
Mysterion186 <kowsikan@hotmail.fr>
2021-11-21 17:12:07 +0100

    maj pour pouvoir ajouter des nom personalisé pour les repo

--- github.sh
+++ github.sh
@@ -4,16 +4,22 @@ pwd
 my_var="$(python3 /Full/Path/To/The/File/tokens.py)" # variable qui va contenir le token 
 var=$(pwd)
 mydir="$(basename $PWD)"
-echo "$mydir"
+# check qu'il n'y a pas de paramètres supplémentaire qui indique qu'on veut donner un nom personalisée
+if [ -z "$2" ]
+	then
+		mydir = "$(basename $PWD)"
+else
+	mydir="$2"
+fi
 echo "$my_var"
 curl -H "Authorization: token "$my_var" " --data '{"name":"'$mydir'","private":'$1'}' https://api.github.com/user/repos
 touch .gitignore
-touch readme.md
-echo .DS_Store >> .gitignore
-echo \#"($mydir)" >> readme.md
+echo \# "$mydir" >> readme.md
 git init
+touch readme.md
 git add readme.md
+echo .DS_Store >> .gitignore
 git commit -m "first commit"
 git branch -M main
-git remote add origin https://github.com/USERNAME/"$mydir".git
+git remote add origin https://github.com/mysterion186/"$mydir".git
 git push -u origin main
